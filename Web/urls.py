13fdb5600d4292cc4360b4de5f002060d35938ef
mansrz <man.srz@gmail.com>
2015-04-23 12:15:05 -0500

    Fix urls

--- Web/urls.py
+++ Web/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ApiRa.views.home', name='home'),
+    #url(r'^$', 'ApiRa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^places/', 'Web.views.PlaceTypes', name='types'),
     url(r'^positions', 'Web.views.Positions', name='positions'),
