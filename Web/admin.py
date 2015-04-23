13732c90f6645510bcd4ea8ccff2d30c19fa3e2c
mansrz <man.srz@gmail.com>
2015-04-23 12:06:57 -0500

    Modify admin for filter in positions

--- Web/admin.py
+++ Web/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from models import PlaceType,Position
 # Register your models here.
 admin.site.register(PlaceType)
-admin.site.register(Position)
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name','latitude','longitude','place')
+    list_filter = ('place__name',)
+
+admin.site.register(Position,PositionAdmin)
 
