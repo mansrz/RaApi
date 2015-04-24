0438ba1a50a83ecf1e435e4ea221af75140c6fbf
Manuel Suárez <man.srz@gmail.com>
2015-04-24 15:01:46 -0500

    Add user model

--- Web/views.py
+++ Web/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.template import RequestContext
 import json
+from django.contrib.auth.decorators import login_required
 
 from models import PlaceType, Position
 # Create your views here.
@@ -75,8 +76,45 @@ def Map(request):
     if request.method == 'GET':
         context = {}
         return render(request, template, context)
-        
+       
     
+@never_cache
+def login(request):
+    #if not request.is_ajax() or request.method != 'GET':
+    #    return
+
+    try:
+        username = request.GET['username']
+        password = request.GET['password']
+    except:
+        return HttpResponseBadRequest('Bad parameters')
+
+    from django.contrib.auth import authenticate, login
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+
+            # End session when the browser is closed. Otherwise use default cookie age
+            if not request.GET.get('remember-me', None):
+                request.session.set_expiry(0)
+
+            response_content = {
+                'username': user.username,
+                'email': user.email,
+                'avatar': user.profile.avatar
+            }
+            response =  HttpResponse(json.dumps(response_content))
+            response['Content-Type'] = 'application/json; charset=utf-8'
+            response['Cache-Control'] = 'no-cache'
+            return response
+        else:
+            # Return a 'disabled account' error message
+            return HttpResponseBadRequest('User have been suspended')
+    else:
+        # Return an 'invalid login' error message.
+        return HttpResponseBadRequest('User and password are incorrect')
 
          
 
