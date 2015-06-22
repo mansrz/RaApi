ef11bdf0ec090f6d4c0faf58d77285ea676c6042
mansrz <man.srz@gmail.com>
2015-06-22 01:19:33 -0500

    Changes in scripts

--- scripts/getpoints.py
+++ scripts/getpoints.py
@@ -1,24 +1,16 @@
 import json
 import urllib2
 
-data = json.load(urllib2.urlopen('http://sigeoespol.cepra.cedia.org.ec:8080/geoserver/Espol3D/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Espol3D:Servicios_P1_FIEC_CENT&maxFeatures=50&outputFormat=application%2Fjson'))
-
-class Point():
-    codigo = ''
-    tipo = ''
-    origen = ''
-    nomen = ''
-    lat = ''
-    lon = ''
-    bloque = ''
-    nombre = ''
-    unidad = ''
-
-points = []
-features = data['features']
-names = []
-for p in features:
-    for d in features['properties']:
-        var =  p['properties'][''+d]
+urls = ['http://sigeoespol.cepra.cedia.org.ec:8080/geoserver/Espol3D/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Espol3D:Servicios_P1_FIEC_CENT&maxFeatures=50&outputFormat=application%2Fjson','http://sigeoespol.cepra.cedia.org.ec:8080/geoserver/Espol3D/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Espol3D:Oficinas_P1_FIEC_CENT&maxFeatures=50&outputFormat=application%2Fjson','http://sigeoespol.cepra.cedia.org.ec:8080/geoserver/Espol3D/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Espol3D:Laboratorio_P1_FIEC_CENT&maxFeatures=50&outputFormat=application%2Fjson','http://sigeoespol.cepra.cedia.org.ec:8080/geoserver/Espol3D/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Espol3D:Infraestructura_de_Equipos_P1_FIEC_CENT&maxFeatures=50&outputFormat=application%2Fjson','http://sigeoespol.cepra.cedia.org.ec:8080/geoserver/Espol3D/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Espol3D:Bloques_FIEC_CENT&maxFeatures=50&outputFormat=application%2Fjson','http://sigeoespol.cepra.cedia.org.ec:8080/geoserver/Espol3D/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Espol3D:Aulas_P1_FIEC_CENT&maxFeatures=50&outputFormat=application%2Fjson']
+for url in urls:
+    data = json.load(urllib2.urlopen(url))
+    points = []
+    features = data['features']
+    names = []
+    for p in data['features']:
+        for d in p['properties']:
+            print d
+            var =  p['properties'][''+d]
+            print var
 
 
