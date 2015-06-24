3a1ab03e2eca1fce476571f09678c044eda593ba
mansrz <man.srz@gmail.com>
2015-06-24 11:20:55 -0500

    Changes in admin

--- Web/admin.py
+++ Web/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from models import PlaceType,Position
+from models import Profile,Position
 # Register your models here.
-admin.site.register(PlaceType)
+admin.site.register(Profile)
 
 class PositionAdmin(admin.ModelAdmin):
-    list_display = ('name','latitude','longitude','place')
-    list_filter = ('place__name',)
+    list_display = ('name','latitude','longitude','profile')
+    list_filter = ('profile__name',)
 
 admin.site.register(Position,PositionAdmin)
 
